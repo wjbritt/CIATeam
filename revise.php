0431e1830739b9e2a9a7a1f617ddeb82e9544253
J Britt <wjbritt@wjbritt.com>
2013-12-15 15:29:16 -0500

    Create revise.php
    Uploading files for Team review

---
+++ revise.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+
+	<head>
+		<title>Revise an employee record</title>
+	</head>
+	<body>
+
+
+<img style="border-width: 0px;" src="http://www.bonder.com/images/under-construction.gif" width="452" height="478" />
+
+	
+
+<br><br>
+<a href="http://www.crosshillmarket.com/index.php">Return Home</a>
+	</body>
+</html>
