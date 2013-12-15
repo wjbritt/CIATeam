fc745f7af019e9e6f24a22bff3a4cce3235254eb
J Britt <wjbritt@wjbritt.com>
2013-12-15 18:31:37 -0500

    Delete db.inc.php
    solution did not work for 2nd attempt at revision method

--- db.inc.php
+++
@@ -1,9 +0,0 @@
-<?
-//file include for database connection
-//db.inc.php
-//database connection
-mysql_connect("Crosshill.db.4683335.hostedresource.com","Crosshill","WJB@Crosshill4");
-
-// database selection
-mysql_select_db("Crosshill");
-?>
