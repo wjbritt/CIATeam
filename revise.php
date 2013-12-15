35aaf3d96d7dc8e081cdbadb9a4e05c5d2dbb15b
J Britt <wjbritt@wjbritt.com>
2013-12-15 16:21:34 -0500

    Update revise.php
    Insert code into page that calls the update.php page

--- revise.php
+++ revise.php
@@ -9,7 +9,34 @@
 
 <img style="border-width: 0px;" src="http://www.bonder.com/images/under-construction.gif" width="452" height="478" />
 
-	
+<?php
+mysql_connect('Crosshill.db.4683335.hostedresource.com', 'Crosshill', 'WJB@Crosshill4') or die(mysql_error());
+mysql_select_db("Employees") or die(mysql_error());
+
+$UID = (int)$_GET['employeeid'];
+$query = mysql_query("SELECT * FROM Employees WHERE id = '$UID'") or die(mysql_error());
+
+if(mysql_num_rows($query)>=1){
+    while($row = mysql_fetch_array($query)) {
+        $firstname = $row['firstname'];
+        $lastname = $row['lastname'];
+        $department = $row['department'];
+     
+    }
+?>
+<form action="update.php" method="post">
+<input type="hidden" name="ID" value="<?=$UID;?>">
+First Name: <input type="text" name="ud_firstname" value="<?=$firstname?>"><br>
+Last Name: <input type="text" name="ud_lastname" value="<?=$lastname?>"><br>
+Department: <input type="text" name="ud_department" value="<?=$department?>"><br>
+<input type="Submit">
+</form>
+<?php
+
+}else{
+    echo 'No entry found. <a href="javascript:history.back()">Go back</a>';
+}
+?>	
 
 <br><br>
 <a href="http://www.crosshillmarket.com/index.php">Return Home</a>
