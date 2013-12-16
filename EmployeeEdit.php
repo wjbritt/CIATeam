8f9ce0e324ea3ea986183bb547d7b1c47b2c01cf
J Britt <wjbritt@wjbritt.com>
2013-12-15 19:16:08 -0500

    Create EmployeeEdit.php

---
+++ EmployeeEdit.php
@@ -0,0 +1,42 @@
+<?php
+
+$username="Crosshill";
+$password="WJB@Crosshill4";
+$database="Crosshill";
+
+mysql_connect("Crosshill.db.4683335.hostedresource.com",$username,$password);
+@mysql_select_db($database) or die ("Unable to select database");
+$employeeid = $_GET[employeeid]; 
+$query = "SELECT * FROM Employees WHERE id = 'employeeid'"; 
+$result = mysql_query($query);
+$row = mysql_fetch_array($result);
+mysql_close();
+?>
+
+<form method="post" action="EditEmployeeData.php" />
+
+<table>
+
+<tr>
+<td><input type="hidden" name="employeeid" value="<? echo "$row[employeeid]" ?>"></td>
+</tr>
+
+<tr>
+<td>First Name:</td>
+<td><input type="text" name="firstname" value="<? echo "$row[firstname]" ?>"></td>
+</tr>
+
+<tr>
+<td>Last Name:</td>
+<td><input type="text" name="lastname" value="<? echo "$row[lastname]" ?>"></td>
+</tr>
+
+<tr>
+<td>Phone Number:</td>
+<td><input type="text" name="department" value="<? echo "$row[department]" ?>"></td>
+</tr>
+
+
+</table>
+
+</form>
